7a640e11150ea735ba9513937b9e116938345b55
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-10-14 11:16:19 +0200

    implement server

--- src/nix_cache_server/server.rs
+++ src/nix_cache_server/server.rs
@@ -1,20 +1,28 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, head, web::Path};
+use crate::git_store::{cache_info, nar_info, store_entry};
+use actix_web::{
+    App, HttpResponse, HttpServer, Responder, get, head,
+    web::{Data, Path},
+};
+use std::sync::Arc;
 use tracing_actix_web::TracingLogger;
 
-// use crate::git_store::GitStore;
+use crate::git_store::GitStore;
 
 #[get("/nix-cache-info")]
 async fn nix_cache_info() -> impl Responder {
-    HttpResponse::Ok().body("hi")
+    let default_cache_info = serde_json::to_vec(&cache_info::CacheInfo::default()).unwrap();
+    HttpResponse::Ok().body(default_cache_info)
 }
 
-#[head("/{nix_hash}.narinfo")]
-async fn get_narinfo(path: Path<String>) -> impl Responder {
+#[get("/{nix_hash}.narinfo")]
+async fn get_narinfo(cache: Data<Arc<GitStore>>, path: Path<String>) -> impl Responder {
+    let cache = cache.into_inner();
     let hash = path.into_inner();
-    if !hash.is_empty() {
-        HttpResponse::Ok()
-    } else {
-        HttpResponse::NotFound()
+    let res = nar_info::get_from_tree(&cache, &hash);
+    match res {
+        Ok(Some(nar_info)) => HttpResponse::Ok().body(nar_info),
+        Ok(None) => HttpResponse::NotFound().body("Entry is not in the Cache"),
+        _ => HttpResponse::InternalServerError().body("Server error while fetching narinfo entry"),
     }
 }
 
@@ -25,25 +33,41 @@ async fn get_listing(path: Path<String>) -> impl Responder {
 }
 
 #[get("/nar/{file_hash}.nar.{compression}")]
-async fn get_compressed_nar(path: Path<(String, String)>) -> impl Responder {
-    let (hash, compression) = path.into_inner();
-    HttpResponse::Ok().body(format!(
-        "Hash: {}\nCompression Method: {}",
-        hash, compression
-    ))
+async fn get_compressed_nar(
+    cache: Data<Arc<GitStore>>,
+    path: Path<(String, String)>,
+) -> impl Responder {
+    let cache = cache.into_inner();
+    let (hash, compression_method) = path.into_inner();
+    if compression_method != "xz" {
+        return HttpResponse::NotFound().body("Only supporting xz compression");
+    }
+    match store_entry::get_as_xz(&cache, &hash) {
+        Ok(Some(nar)) => HttpResponse::Ok().body(nar),
+        Ok(None) => HttpResponse::NotFound().body("Entry is not in the Cache"),
+        _ => HttpResponse::InternalServerError().body("Server error while fetching entry"),
+    }
 }
 
-#[get("/{nix_hash}.narinfo")]
-async fn nar_exists(path: Path<String>) -> impl Responder {
+#[head("/{nix_hash}.narinfo")]
+async fn nar_exists(cache: Data<Arc<GitStore>>, path: Path<String>) -> impl Responder {
+    let cache = cache.into_inner();
     let hash = path.into_inner();
-    HttpResponse::Ok().body(hash)
+
+    if nar_info::exists(&cache, &hash) {
+        HttpResponse::Ok()
+    } else {
+        HttpResponse::NotFound()
+    }
 }
 
 #[actix_web::main]
-pub async fn start_server(host: &str, port: u16) -> std::io::Result<()> {
+pub async fn start_server(host: &str, port: u16, cache: GitStore) -> std::io::Result<()> {
+    let cache = Arc::new(cache);
     HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
+            .app_data(Data::new(cache.clone()))
             .service(get_narinfo)
             .service(nix_cache_info)
             .service(nar_exists)
