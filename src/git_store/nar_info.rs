e07e660f4390f42455780e8425296bd13ca0c908
Ephraim Siegfried <ephraim.siegfried@proton.me>
2025-10-28 08:13:04 +0100

    use references in nar info and extend new function

--- src/git_store/nar_info.rs
+++ src/git_store/nar_info.rs
@@ -1,42 +1,51 @@
 use std::{fmt::Display, fs, path::PathBuf};
 
-use crate::git_store::GitStore;
+use crate::git_store::GitRepo;
 use anyhow::Result;
 use nix_base32::to_nix_base32;
 
 const NARINFO_REF: &str = "refs/NARINFO";
 
-pub struct NarInfo {
-    store_path: String,
-    url: String,
-    compression_type: String,
-    file_hash: Vec<u8>,
+pub struct NarInfo<'a> {
+    store_path: &'a str,
+    url: &'a str,
+    compression_type: Option<&'a str>,
+    file_hash: &'a [u8],
     file_size: u64,
-    nar_hash: Vec<u8>,
+    nar_hash: &'a [u8],
     nar_size: u64,
-    deriver: String,
-    system: String,
-    references: Vec<String>,
+    deriver: Option<&'a str>,
+    system: &'a str,
+    references: Vec<&'a str>,
 }
 
-impl NarInfo {
-    pub fn new(store_path: String, url: String) -> Self {
+impl<'a> NarInfo<'a> {
+    pub fn new(
+        store_path: &'a str,
+        url: &'a str,
+        file_size: u64,
+        compression_type: Option<&'a str>,
+        nar_hash: &'a [u8],
+        nar_size: u64,
+        deriver: Option<&'a str>,
+        references: Vec<&'a str>,
+    ) -> Self {
         Self {
             store_path: store_path,
             url: url,
-            compression_type: "".to_string(),
-            file_hash: Vec::new(),
-            file_size: 0,
-            nar_hash: Vec::new(),
-            nar_size: 0,
-            deriver: "".to_string(),
-            system: "".to_string(),
-            references: Vec::new(),
+            compression_type: compression_type,
+            file_hash: &[],
+            file_size: file_size,
+            nar_hash: nar_hash,
+            nar_size: nar_size,
+            deriver: deriver,
+            system: "", // TODO: Get system
+            references: references,
         }
     }
 }
 
-impl Display for NarInfo {
+impl<'a> Display for NarInfo<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let keys = [
             "StorePath",
@@ -50,18 +59,20 @@ impl Display for NarInfo {
             "References",
             "Sig",
         ];
+        let file_size = self.file_size.to_string();
+        let nar_size = self.nar_size.to_string();
         let values = [
-            &self.store_path,
-            &self.url,
-            &self.compression_type,
+            self.store_path,
+            self.url,
+            self.compression_type.unwrap_or(""),
             "",
             // &to_nix_base32(&self.file_hash),
-            &self.file_size.to_string(),
+            file_size.as_str(),
             "",
             // &to_nix_base32(&self.nar_hash),
-            &self.nar_size.to_string(),
-            &self.deriver,
-            &self.system,
+            nar_size.as_str(),
+            self.deriver.unwrap_or(""),
+            self.system,
             &self.references.join(" "),
         ];
         for (key, value) in keys.iter().zip(values) {
@@ -71,20 +82,20 @@ impl Display for NarInfo {
     }
 }
 
-pub fn get_from_tree(cache: &GitStore, key: &str) -> Result<Option<Vec<u8>>> {
+pub fn get_from_tree(cache: &GitRepo, key: &str) -> Result<Option<Vec<u8>>> {
     cache.get_blob(key, NARINFO_REF)
 }
 
-pub fn add_file(cache: &GitStore, path: &PathBuf, key: &str) -> Result<()> {
+pub fn add_file(cache: &GitRepo, path: &PathBuf, key: &str) -> Result<()> {
     let content = fs::read(path)?;
     cache.add_file_content(key, &content, NARINFO_REF)?;
     Ok(())
 }
 
-pub fn exists(cache: &GitStore, key: &str) -> bool {
+pub fn exists(cache: &GitRepo, key: &str) -> bool {
     cache.query(key, NARINFO_REF).is_some()
 }
-pub fn list(cache: &GitStore) -> Result<Vec<String>> {
+pub fn list(cache: &GitRepo) -> Result<Vec<String>> {
     cache.list_keys(NARINFO_REF)
 }
 
