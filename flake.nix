9e611785d3287897a37d2a3c97d0d926d02bd11f
Ephraim Siegfried <ephraim.siegfried@proton.me>
2025-11-06 08:17:57 +0100

    fix flake

--- flake.nix
+++ flake.nix
@@ -21,24 +21,42 @@
 
         naersk' = pkgs.callPackage naersk { };
 
+        runtimeLibs = with pkgs; [
+          openssl
+          libssh2
+        ];
+
+        pkgConfigDeps = with pkgs; [
+          openssl.dev
+          libssh2
+        ];
+
+        buildTools = with pkgs; [
+          pkg-config
+          rustc
+          cargo
+        ];
+
         cargoConfig = {
-          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+          PKG_CONFIG_PATH = pkgs.lib.makeSearchPath "lib/pkgconfig" pkgConfigDeps;
           OPENSSL_NO_VENDOR = 1;
           OPENSSL_DIR = "${pkgs.openssl.dev}";
           OPENSSL_LIB_DIR = "${pkgs.lib.getLib pkgs.openssl}/lib";
         };
       in
       {
-        defaultPackage =
-          naersk'.buildPackage {
+        defaultPackage = naersk'.buildPackage (
+          {
             src = ./.;
+
+            buildInputs = runtimeLibs;
+            nativeBuildInputs = [ pkgs.pkg-config ];
           }
-          // cargoConfig;
+          // cargoConfig
+        );
+
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            rustc
-            cargo
-          ];
+          nativeBuildInputs = buildTools ++ runtimeLibs;
           env = cargoConfig;
         };
       }
