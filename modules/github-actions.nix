a6309c368526813b24020252aba2e430e9e4561a
Tom van Dijk <18gatenmaker6@gmail.com>
2026-01-31 22:49:47 +0100

    ci: only run build-check on other systems than `x86_64-linux`

--- modules/github-actions.nix
+++ modules/github-actions.nix
@@ -1,4 +1,16 @@
-{ self, inputs, ... }:
 {
-  flake.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix { checks = self.checks; };
+  self,
+  lib,
+  inputs,
+  ...
+}:
+{
+  flake.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
+    checks = {
+      inherit (self.checks) x86_64-linux;
+    }
+    // lib.genAttrs [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" ] (system: {
+      inherit (self.checks.${system}) build-check;
+    });
+  };
 }
